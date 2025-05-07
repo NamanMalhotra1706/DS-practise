eb0e5aab8e9cc04fd1b280f98af49a822c4c8fe7
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-05-07 23:27:34 +0530

    [Naman Malhotra] ADD: Java Hashset

--- JavaHashset.java
+++ JavaHashset.java
@@ -1,2 +1,44 @@
+
+// Hackerrank Question Link : https://www.hackerrank.com/challenges/java-hashset/problem
+import java.util.*;
+
+
 public class JavaHashset {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        HashMap<String, HashSet<String>> map = new HashMap<>();
+
+        int setEleCounts = 0;
+
+        for(int i=0;i< t ; i++){
+            if(map.containsKey(pair_left[i])){
+                HashSet<String> st = map.get(pair_left[i]);
+                if(!st.contains(pair_right[i])){
+                    st.add(pair_right[i]);
+                    setEleCounts += 1;
+                }
+            }
+            else{
+                HashSet<String> newSet = new HashSet<>();
+                newSet.add(pair_right[i]);
+                map.put(pair_left[i], newSet);
+                setEleCounts +=1;
+            }
+
+            System.out.println(setEleCounts);
+
+        }
+
+
+    }
 }
